411fd5071e8f611ee3e308bb66b460a98497764c
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-06-05 23:42:22 +0530

    Initial Commit

---
+++ PasswordManager/PasswordManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerApp.swift
+//  PasswordManager
+//
+//  Created by Hitesh Madaan on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
