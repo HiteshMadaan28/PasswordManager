649547270de215a7372fe2fca8b207a3546da3d7
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-06-06 01:53:13 +0530

    Added Biometric Authentication (FaceID)

--- PasswordManager/ContentView.swift
+++ PasswordManager/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import LocalAuthentication
+
 
 struct AddNewAccount:View {
     var body: some View {
@@ -19,26 +21,42 @@ struct AddNewAccount:View {
 
 struct ContentView: View {
     @State private var accounts:[String]=[]
+    @State private var unlocked:Bool=false
+    @State private var text="LOCKED"
     @State private var accountName=""
     @State private var userName=""
     @State private var password=""
     @State private var showAddingSheet: Bool = false
     var body: some View {
         NavigationStack{
-            List{
-                ForEach(accounts,id:\.self){
-                    Text("\($0)")
+            VStack {
+                if unlocked {
+                    Text("Unlocked")
+                    List{
+                        ForEach(accounts,id:\.self){
+                            Text("\($0)")
+                        }
+                        
+                        Text(text)
+                            .bold()
+                            .padding()
+                        
+                    }
+                    
+                    Button("Click"){
+                        showAddingSheet.toggle()
+                    }
+                    .foregroundColor(.red)
+                    .sheet(isPresented:$showAddingSheet){
+                        AddNewAccount()
+                            .presentationDetents([.fraction(0.4),.large])
+                            .presentationDragIndicator(.visible)
+                    }
+                } else {
+                    Text("Locked")
                 }
             }
-            
-            Button("Click"){
-                showAddingSheet.toggle()
-            }
-            .sheet(isPresented:$showAddingSheet){
-                AddNewAccount()
-                    .presentationDetents([.fraction(0.4),.large])
-                    .presentationDragIndicator(.visible)
-            }
+            .onAppear(perform: authenticate)
                 
             .navigationTitle("Password Manager")
             
@@ -46,6 +64,29 @@ struct ContentView: View {
         }
         
     }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    unlocked=true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+
 }
 
 #Preview {
