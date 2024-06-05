20ac2efa2fd2a04c7ac8a337232bf29717d8e1b5
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-06-06 00:53:18 +0530

    Project started

--- PasswordManager/ContentView.swift
+++ PasswordManager/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AddNewAccount:View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:20){
+            Text("Account Name")
+            Image(systemName: "star")
+                .padding()
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var accounts:[String]=[]
+    @State private var accountName=""
+    @State private var userName=""
+    @State private var password=""
+    @State private var showAddingSheet: Bool = false
+    var body: some View {
+        NavigationStack{
+            List{
+                ForEach(accounts,id:\.self){
+                    Text("\($0)")
+                }
+            }
+            
+            Button("Click"){
+                showAddingSheet.toggle()
+            }
+            .sheet(isPresented:$showAddingSheet){
+                AddNewAccount()
+                    .presentationDetents([.fraction(0.4),.large])
+                    .presentationDragIndicator(.visible)
+            }
+                
+            .navigationTitle("Password Manager")
+            
+            
+        }
+        
     }
 }
 
